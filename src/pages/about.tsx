f323aaa0a00ff5771bf83aff6ba8752af4de504e
Fudy-V <fudyV.test@gmail.com>
2024-10-20 02:21:32 +0900

    ♻️ font設定の分割

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,9 @@
 import Head from "next/head";
-import localFont from "next/font/local";
+import { geistSans, geistMono } from "@/pages/fonts";
 import classes from "@/styles/Home.module.css";
 import { Footer } from "@/compornents/Fotter";
 import { Main } from "@/compornents/Main";
 import { Header } from "@/compornents/Header";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
