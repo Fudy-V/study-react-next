d5181b53cf43717a3a6f8d4b65440179a74ac1f2
Fudy-V <fudyV.test@gmail.com>
2024-10-20 13:55:23 +0900

    ✨ useEffectの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,22 @@ import classes from "@/styles/Home.module.css";
 import { Footer } from "@/compornents/Fotter";
 import { Main } from "@/compornents/Main";
 import { Header } from "@/compornents/Header";
+import { useEffect, useCallback } from "react";
 import Link from "next/link";
-import { useCallback } from "react";
 import { eventType } from "@/types";
 
 export default function Home() {
   const handleClick = useCallback((e: eventType) => {
     e.preventDefault();
+  }, []);
+
+  // 処理→マウント時、返り値→アンマウント時の処理
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
